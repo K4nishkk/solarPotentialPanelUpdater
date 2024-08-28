87772050bce973444bd223777d770cd0cf1d7546
K4nishkk <k4nishkk@gmail.com>
2024-08-29 00:41:28 +0530

    m

--- api/hello.js
+++ api/hello.js
@@ -1,6 +1,7 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request) {
-    console.log("function ran")
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+    setInterval(() => {
+        console.log("something");
+    }, 1000); // 1000 milliseconds = 1 second
 }
