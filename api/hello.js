707043d889ec9fc5a3a4ee0fbf23875e40d4a523
K4nishkk <k4nishkk@gmail.com>
2024-08-29 01:10:19 +0530

    m

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
-export function GET(request) {
-    setInterval(() => {
-        console.log("something");
-    }, 1000); // 1000 milliseconds = 1 second
-}
