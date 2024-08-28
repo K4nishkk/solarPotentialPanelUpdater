6fcd288b27638f9d975730db1eff80e01051763f
K4nishkk <k4nishkk@gmail.com>
2024-08-29 01:36:59 +0530

    message

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request) {
-    console.log("function ran")
+    console.log("new function ran")
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
