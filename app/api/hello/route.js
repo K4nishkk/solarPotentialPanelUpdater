c6bdcef785be834cf7448c4ee11a7d4eed49f011
K4nishkk <k4nishkk@gmail.com>
2024-09-07 12:49:45 +0530

    get product desc, image and price

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,52 +1,111 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  const url1 = 'https://www.loomsolar.com/collections/solar-panels';
-  const url2 = 'https://shop.waaree.com/solar-module/';
-  const url3 = 'https://www.upsinverter.com/product-category/solar-solutions/solar-panel/';
+  const productsCollectionPages = [
+    {
+      site: "loomsolar",
+      url: 'https://www.loomsolar.com/collections/solar-panels',
+      selector: '.product-item__title',
+      areLinksRelative: true,
+      baseURL: 'https://www.loomsolar.com'
+    },
+    {
+      site: "waaree",
+      url: 'https://shop.waaree.com/solar-module/',
+      selector: '.card-title a',
+      areLinksRelative: false
+    },
+    {
+      site: "upsinverter",
+      url: 'https://www.upsinverter.com/product-category/solar-solutions/solar-panel/',
+      selector: '.woocommerce-LoopProduct-link',
+      areLinksRelative: false
+    }
+  ]
 
-  try {
-    const productLinks = [];
+  const productLinks = [];
 
-    const response1 = await axios.get(url1);
-    const data1 = cheerio.load(response1.data);
-    data1('.product-item__title').each((index, element) => {
-      const link = data1(element).attr('href');  // Extract the href attribute
-      if (link) {
-        productLinks.push("https://www.loomsolar.com" + link);  // Add to the list if href exists
-      }
-    });
-
-    const response2 = await axios.get(url2);
-    const data2 = cheerio.load(response2.data);
-    data2('.card-title a').each((index, element) => {
-      const link = data2(element).attr('href');  // Extract the href attribute
-      if (link) {
-        productLinks.push(link);  // Add to the list if href exists
-      }
-    });
-
-    const response3 = await axios.get(url3);
-    const data3 = cheerio.load(response3.data);
-    data3('.woocommerce-LoopProduct-link').each((index, element) => {
-      const link = data3(element).attr('href');  // Extract the href attribute
-      if (link) {
-        productLinks.push(link);  // Add to the list if href exists
-      }
-    });
-    
-    console.log({ links: productLinks })
-    return new Response(JSON.stringify({ links: productLinks }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (error) {
-    console.error(`Error scraping the data: ${error.message}`);
-    return new Response(JSON.stringify({ error: 'Error scraping the data' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    });
+  for (const collectionPage of productsCollectionPages) {
+    try {
+      const response = await axios.get(collectionPage.url);
+      const $ = cheerio.load(response.data);
+      $(collectionPage.selector).each((index, element) => {
+        const link = $(element).attr('href');
+        if (link) {
+          productLinks.push({
+            site: collectionPage.site,
+            url: collectionPage.areLinksRelative ? collectionPage.baseURL + link : link
+          });
+        }
+      });
+    }
+    catch (error) {
+      console.error(`Error scraping the data: ${error.message}`);
+      return new Response(JSON.stringify({ error: 'Error scraping the data' }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' },
+      });
+    }
   }
+
+  const productData = [];
+
+  const selectors = {
+    loomsolar: {
+      text: '.rte.text--pull',
+      img: '.product-gallery__image',
+      price: '.price.price--highlight'
+    },
+    waaree: {
+      text: '.productView-info',
+      img: '.productView-image--default',
+      price: '.productView-price .price.price--withTax'
+    },
+    upsinverter: {
+      text: '.woocommerce-product-details__short-description',
+      img: '.wp-post-image',
+      price: '.woocommerce-Price-amount.amount'
+    }
+  }
+
+  for (const link of productLinks) {
+    try {
+      const response = await axios.get(link.url);
+      const $ = cheerio.load(response.data);
+      
+      const rawText = $(selectors[link.site].text).text().trim();
+      const text = rawText.replace(/\s+/g, ' ');
+
+      var img;
+      if (link.site === "loomsolar") {
+        const unsizedImage = $(selectors[link.site].img).attr('data-src')
+        img = unsizedImage.replace("{width}", "600")
+      }
+      else {
+        img = $(selectors[link.site].img).attr('src')
+      }
+
+      const price = $(selectors[link.site].price).first().text().trim();
+
+      productData.push({text: text, img: img, price: price});
+      // const genAI = new GoogleGenerativeAI("AIzaSyAtw3N_dAJo3_zKrz7X42aaN1krTRRkxbA");
+      // const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
   
+      // const prompt = "Analyse this text an write it in structured format" + text;
+  
+      // const result = await model.generateContent(prompt);
+      // console.log(result.response.text());
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+
+ 
+
+  return new Response(JSON.stringify({ productData }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
--- app/page.js
+++ app/page.js
@@ -1,32 +1,6 @@
-'use client'
-
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-
 export default function Home() {
-  const [titles, setTitles] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the API route on your server
-    axios.get('/api/hello')
-      .then((response) => {
-        setTitles(response.data.titles);
-      })
-      .catch((error) => {
-        console.error(`Error: ${error.message}`);
-      });
-  }, []);
 
   return (
-    <div>
-      <h1>Product Titles</h1>
-      <ul>
-        {titles.length > 0 ? (
-          titles.map((title, index) => <li key={index}>{title}</li>)
-        ) : (
-          <li>Loading...</li>
-        )}
-      </ul>
-    </div>
+    <div>Home</div>
   );
 }
