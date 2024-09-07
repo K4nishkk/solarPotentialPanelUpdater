eb243439e22316ac37cdfbb34d4d92478d698351
K4nishkk <k4nishkk@gmail.com>
2024-09-07 09:36:20 +0530

    added multiple urls

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -3,23 +3,42 @@ import * as cheerio from 'cheerio';
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
-  const url = 'https://www.loomsolar.com/collections/solar-panels';
+  const url1 = 'https://www.loomsolar.com/collections/solar-panels';
+  const url2 = 'https://shop.waaree.com/solar-module/';
+  const url3 = 'https://www.upsinverter.com/product-category/solar-solutions/solar-panel/';
 
   try {
-    const response = await axios.get(url);
-    const $ = cheerio.load(response.data); // Use cheerio.load to parse the HTML
-    const products = $('.product-item');
+    const productLinks = [];
 
-    const productTitles = [];
-
-    products.each((index, product) => {
-      const title = $(product).find('a.product-item__title').text().trim();
-      productTitles.push(title);
+    const response1 = await axios.get(url1);
+    const data1 = cheerio.load(response1.data);
+    data1('.product-item__title').each((index, element) => {
+      const link = data1(element).attr('href');  // Extract the href attribute
+      if (link) {
+        productLinks.push("https://www.loomsolar.com" + link);  // Add to the list if href exists
+      }
     });
 
-    // Send the scraped data back as JSON
-    console.log({ titles: productTitles })
-    return new Response(JSON.stringify({ titles: productTitles }), {
+    const response2 = await axios.get(url2);
+    const data2 = cheerio.load(response2.data);
+    data2('.card-title a').each((index, element) => {
+      const link = data2(element).attr('href');  // Extract the href attribute
+      if (link) {
+        productLinks.push(link);  // Add to the list if href exists
+      }
+    });
+
+    const response3 = await axios.get(url3);
+    const data3 = cheerio.load(response3.data);
+    data3('.woocommerce-LoopProduct-link').each((index, element) => {
+      const link = data3(element).attr('href');  // Extract the href attribute
+      if (link) {
+        productLinks.push(link);  // Add to the list if href exists
+      }
+    });
+    
+    console.log({ links: productLinks })
+    return new Response(JSON.stringify({ links: productLinks }), {
       headers: { 'Content-Type': 'application/json' },
     });
   } catch (error) {
@@ -29,4 +48,5 @@ export async function GET() {
       headers: { 'Content-Type': 'application/json' },
     });
   }
+  
 }
